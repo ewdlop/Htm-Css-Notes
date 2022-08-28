d41c63811770ca4189f2fd34e1d73293c44a759f
ewdlop <ray810815@gmail.com>
2022-08-28 16:39:03 +0000

    Update 4 files

--- src/components/box.jsx
+++ src/components/box.jsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { keyframes } from "styled-components";
+
 
 export const Box = () => {
-  return <h1> Box </h1>;
+    const container = {
+        display: "flex",
+        flexFlow: "row wrap"
+    };
+    const item = {
+      color: "dark",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Arial"
+    };
+    
+    const spin = keyframes`
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }`;
+    
+    const grow = {
+        ...item,
+        flexGrow: 4,
+        backgroundColor: "red",
+    };
+    
+    const shrink = {
+        ...item,
+        flexShrink: 2,
+        backgroundColor: "green",
+    };
+    
+    
+    
+    return <div style={container}>
+        <div style={item}>Apple</div>
+        <div style={grow}>Orange</div>
+        <div style={shrink}>Lemon</div>
+        <div style={item}>Tomato</div>
+        <div style={item}>Banana</div>
+    </div>
 };
